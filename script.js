9577348485e572576de3309e38af913f00a4c191
Natanael-Aguiarr <157542780+Natanael-Aguiarr@users.noreply.github.com>
2024-10-31 07:44:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const menu = document.querySelector ('.menu');
-const NavMenu = document.querySelector ('.nav-menu');
+const menu=document.querySelector (".menu");
+const NavMenu=document.querySelector (".nav-menu");
 
-menu.addEventListener ('click', ()=> {
-    menu.classList.toogle ('ativo');
-    NavMenu.classList.toogle ('ativo');
+menu.addEventListener ("click", ()=> {
+menu.classList.toogle("ativo");
+NavMenu.classList.toogle("ativo");
+})
+
+let trilho=document.getElementById ("trilho");
+let body=document.querySelector ("body");
+
+trilho.addEventListener ("click", ()=> {
+trilho.classList.toggle ("dark");
+body.classList.toggle ("dark");
 })
